fd50da8a7c6ae754f1deba399252572e71fc908a
ArpitKaushik7 <arpitkaushik10985@gmail.com>
2025-05-12 18:16:08 +0530

    tested the database using docker

---
+++ test_db.py
@@ -0,0 +1,19 @@
+import psycopg2
+
+conn = psycopg2.connect(
+    dbname="attendance",
+    user="admin",
+    password="admin123",
+    host="localhost",
+    port="5432"
+)
+
+cur = conn.cursor()
+cur.execute("SELECT * FROM users;")
+rows = cur.fetchall()
+
+for row in rows:
+    print(row)
+
+cur.close()
+conn.close()
